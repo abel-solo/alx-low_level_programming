d527c525887c418c4c8cdffdf9a7354fc8af473a
abel-solo <abel.solo2011@yahoo.com>
2022-06-06 12:38:28 -0700

    prints alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase, followed by a new line
+ * Return: 0 when runs succesfully
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; ,++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
