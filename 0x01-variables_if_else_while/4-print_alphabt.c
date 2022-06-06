6e36fcbc219014247a6b2ab4181224cc46fd2929
abel-solo <abel.solo2011@yahoo.com>
2022-06-06 14:04:05 -0700

    alphabet

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
- * main -  prints the alphabet in lowercase, followed by a new line
+ * main - prints the alphabet in lowercase, followed by a new line
  * Return:0 when runs succesfully;
  */
 int main(void);
