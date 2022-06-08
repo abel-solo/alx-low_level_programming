ce6088b785713a981955cdf413c928798f5a8106
abel-solo <abel.solo2011@yahoo.com>
2022-06-08 01:47:10 -0700

    lowercase alphabet

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase character
+ * @c: the paramater
+ * Return: the output
+ */
+int _islower(int c)
+{
+	int c;
+
+	return (c >= 'a' && c <= 'z');
+}
