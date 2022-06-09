141d511180b1796eb6f4c23190693e7a28d803c1
abel-solo <abel.solo2011@yahoo.com>
2022-06-09 13:45:41 -0700

    uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#includ "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: the checked character
+ * Return: 1 if c is uppercase 0 otherwise
+ */
+int _isupper(int c)
+{
+		return (c >= 'A' && c <= 'Z');
+
+}
+
