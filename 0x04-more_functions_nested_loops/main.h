3da7100ac0c9d4cdea299c787bdaed62282f1b27
abel-solo <abel.solo2011@yahoo.com>
2022-06-10 12:55:56 -0700

    main.h modified

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -5,3 +8,5 @@ void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
+
+#endif
