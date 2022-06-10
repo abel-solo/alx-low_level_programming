66565e5c8b058b5897ef161fdb5f4a582a16b2c5
abel-solo <abel.solo2011@yahoo.com>
2022-06-10 13:33:23 -0700

    putchar aded in header file

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,4 @@
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -5,4 +6,3 @@ void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
-
