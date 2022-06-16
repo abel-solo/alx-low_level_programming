182c7a49ed8ca8988a81313a551bf7ed0f5a5977
abel-solo <abel.solo2011@yahoo.com>
2022-06-15 17:21:43 -0700

    adding an integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,28 +1,24 @@
 #include "main.h"
 
 /**
- * print_number -  prints an integer
- * @n: the integer to be printed
- * Return: 0
+ * print_number - prints an integer;
+ * @n: the integer to be printed;
  */
 void print_number(int n)
 {
-	int d = 1;
-	int i, r;
+	unsigned int x;
 
-if (n < 0)
-{
-	_putchar('-');
-	n *= -1;
-}
+	if (n < 0)
+	{
+		x = -n;
+		_putchar('-');
+	} else
+	{
+		x = n;
+	}
 
-for (i = 0; n / d > 9; i++, d *= 10)
-;
-for (; divisor >= 1; n %= divisor, divisor /= 10)
-{
-	resp = n / divisor;
-	_putchar('0' + resp);
+	if (x / 10)
+		print_number(x / 10);
 
-}
-
+	_putchar((x % 10) + '0');
 }
