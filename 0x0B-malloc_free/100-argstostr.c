6c2bb10e655ba2117b1dcf5b8c25ec5acabd0aa5
abel-solo <abel.solo2011@yahoo.com>
2022-06-28 15:09:34 -0700

    adding a space to the assignment oprator

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -27,16 +27,16 @@ char *argstostr(int ac, char **av)
 	if (str == NULL)
 		return (NULL);
 
-	k= 0;
+	k = 0;
 
 	for (i = 0; i < ac; i++)
 	{
-		for (j =0; av[i][j] != '\0'; j++)
+		for (j = 0; av[i][j] != '\0'; j++)
 		{
 			str[k] = av[i][j];
 			k++;
 		}
-		str[k] ='\n';
+		str[k] = '\n';
 		k++;
 	}
 	return (str);
