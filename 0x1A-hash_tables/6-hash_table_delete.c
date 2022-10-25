919c621a959285b1affa472ee5082e77032c566d
abel-solo <abel.solo2011@yahoo.com>
2022-10-25 12:46:04 -0700

    redo the whole code again

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -18,7 +18,7 @@ void hash_table_delete(hash_table_t *ht)
 		return;
 	}
 
-	while (idx < ha->size)
+	while (idx < ht->size)
 	{
 		node = (ht->array)[idx];
 		while (node)
@@ -35,6 +35,6 @@ void hash_table_delete(hash_table_t *ht)
 		}
 		idx++;
 	}
-	free(ht);
 	free(ht->array);
+	free(ht);
 }
