88b17cd5f1344fdeaf7729c9304f5d2904cf5c14
abel-solo <abel.solo2011@yahoo.com>
2022-11-09 13:48:24 -0800

    ading proper comment

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -20,3 +20,16 @@ def island_perimeter(grid):
                     p += 1  # right
     return p
 
+# Method for rectangular island only, no odd shapes
+#    maxWidth = 0
+#    length = 0
+#    for i in range(len(grid)):
+#        width = 0
+#        for j in range(len(grid[0])):
+#            if grid[i][j] == 1:
+#                width += 1
+#        if width:
+#            length += 1
+#        if width > maxWidth:
+#            maxWidth = width
+#    return ((maxWidth + length) * 2)
